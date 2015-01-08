22ccd724a8c7ec5a80f4674746e0c3fce278de6f
Tom <thomas.zumsteg@gmail.com>
2015-01-07 17:08:40 -0800

    First itteration clock

--- clock/clock.h
+++ clock/clock.h
@@ -7,15 +7,25 @@
 namespace date_independent {
 	using namespace std;
 	class clock;
+
+	bool operator== (const clock &clkOne, const clock &clkTwo);
+	bool operator!= (const clock &clkOne, const clock &clkTwo);
 }
 
+// Goes in clock.cpp for any sane project
 class date_independent::clock {
+	/* Keeps track of time */
 private:
 	int m_nHour, m_nMin;
 	clock(int nHour, int nMin) : m_nHour(nHour), m_nMin(nMin) {
-		m_nHour += m_nMin / 60;
-		m_nMin %= 60;
-		m_nHour %= 12;
+		// Substitute for positive modulus (e.g. -10 % 3 == 2)
+		// Probably a better way to do this
+		while (m_nMin >= 60 || m_nMin < 0) {
+			m_nHour += (m_nMin < 0) ?  -1 :  +1;
+			m_nMin  += (m_nMin < 0) ? +60 : -60;
+		}
+		while (m_nHour >= 24 || m_nHour < 0)
+			m_nHour += (m_nHour < 0) ? +24 : -24;
 	}
 public:
 	static clock at(int nHour, int nMin) { return clock(nHour, nMin); }
@@ -27,8 +37,20 @@ public:
 
 	operator string() const {
 		stringstream strTime;
+		// Zero padded two digit displays (e.g. 03:23 or 10:01)
 		strTime << setfill('0') << setw(2) << m_nHour << ":";
 		strTime << setfill('0') << setw(2) << m_nMin;
 		return strTime.str();
 	}
+
+	friend bool date_independent::operator== (const clock &clkOne, const clock &clkTwo);
+	friend bool date_independent::operator!= (const clock &clkOne, const clock &clkTwo);
 };
+
+bool date_independent::operator== (const clock &clkOne, const clock &clkTwo) {
+	return (clkOne.m_nHour == clkTwo.m_nHour) && (clkOne.m_nMin == clkTwo.m_nMin);
+}
+
+bool date_independent::operator!= (const clock &clkOne, const clock &clkTwo) {
+	return !date_independent::operator== (clkOne, clkTwo);
+}
