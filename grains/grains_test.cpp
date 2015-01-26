f66eba05545b13d24e6b5e81be672906fc35b02c
Tom <thomas.zumsteg@gmail.com>
2015-01-25 20:14:12 -0800

    Changes to grains

--- grains/grains_test.cpp
+++ grains/grains_test.cpp
@@ -7,7 +7,6 @@ BOOST_AUTO_TEST_CASE(square_1)
     BOOST_REQUIRE_EQUAL(1ULL, grains::square(1));
 }
 
-//#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(square_2)
 {
     BOOST_REQUIRE_EQUAL(2ULL, grains::square(2));
@@ -38,8 +37,9 @@ BOOST_AUTO_TEST_CASE(square_64)
     BOOST_REQUIRE_EQUAL(9223372036854775808ULL, grains::square(64));
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(total)
 {
     BOOST_REQUIRE_EQUAL(18446744073709551615ULL, grains::total());
 }
-//#endif
+#endif
