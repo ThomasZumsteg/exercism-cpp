4b327b57d151ce3e95591137fe93363334afcac9
ThomasZumsteg <thomas.zumsteg@gmail.com>
2015-02-15 22:04:52 -0800

    difference-of-squares inlined and removed std

--- difference-of-squares/difference_of_squares.h
+++ difference-of-squares/difference_of_squares.h
@@ -3,7 +3,6 @@
 #include <cmath>
 
 namespace squares {
-	using namespace std;
 	/*
 	Sequence substitutions are derived from 
 	sum( f(k), 0, n) = sum( f(k+1), 0, n) - f(n+1)
@@ -23,19 +22,19 @@ namespace squares {
 	sum(k**2) is left as an exercise to the reader.
 		*/
 
-	int square_of_sums(int nNum) {
+	inline int square_of_sums(int nNum) {
 		/* Square of the the sum of numbers */
 		// ((n+1)*n/2)**2
-		return pow(nNum, 2) * pow(nNum + 1, 2) / 4;
+        return pow(nNum, 2) * pow(nNum + 1, 2) / 4;
 	}
 
-	int sum_of_squares(int nNum) {
+	inline int sum_of_squares(int nNum) {
 		/* Sum of the square of numbers */
 		// (2*n**3 + 3*n**2 + n) / 6
 		return nNum * (nNum + 1) * (2 * nNum + 1) / 6;
 	}
 
-	int difference(int nNum) {
+	inline int difference(int nNum) {
 		/* Difference between the square of the sum of numbers and the sum of the square of numbers*/
 		// square_of_sums - sum_of_squares
 		// subtraction left as an exercise
