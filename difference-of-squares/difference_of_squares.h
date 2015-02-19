3d5103118e52f66fa93e3a0af7d65550e112ca50
Tom <thomas.zumsteg@gmail.com>
2015-02-18 16:09:02 -0800

    Readability and std::pow added to difference_of_squares

--- difference-of-squares/difference_of_squares.h
+++ difference-of-squares/difference_of_squares.h
@@ -3,7 +3,6 @@
 #include <cmath>
 
 namespace squares {
-	using namespace std;
 	/*
 	Sequence substitutions are derived from 
 	sum( f(k), 0, n) = sum( f(k+1), 0, n) - f(n+1)
@@ -26,7 +25,7 @@ namespace squares {
 	int square_of_sums(int nNum) {
 		/* Square of the the sum of numbers */
 		// ((n+1)*n/2)**2
-		return pow(nNum, 2) * pow(nNum + 1, 2) / 4;
+		return std::pow(nNum, 2) * std::pow(nNum + 1, 2) / 4;
 	}
 
 	int sum_of_squares(int nNum) {
@@ -37,8 +36,6 @@ namespace squares {
 
 	int difference(int nNum) {
 		/* Difference between the square of the sum of numbers and the sum of the square of numbers*/
-		// square_of_sums - sum_of_squares
-		// subtraction left as an exercise
-		return nNum * (nNum + 1) * (nNum - 1 ) * ( 3 * nNum + 2) / 12;
+		return square_of_sums(nNum) - sum_of_squares(nNum);
 	}
 }
